c9e6ddec79fa643d54b47724dc98491e015e6d9f
uyuyuuy <842276675@qq.com>
2019-06-13 08:28:14 +0800

    add

---
+++ channel.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	//"os"
+	"time"
+)
+
+var count1 int = 1
+
+func main() {
+	//var ch1 chan int
+	//var ch2 map[string] chan int
+	//var ch3 chan [10]int
+	ch4 := make([]chan int, 10)
+
+	for i:=0; i<10 ;i++  {
+		ch4[i] = make(chan int)
+		go Count1(ch4[i], i)
+	}
+	for _,ch := range ch4{
+		time.Sleep(1*time.Second)
+		val := <- ch
+		fmt.Println(count1, val)
+	}
+
+	//time.Sleep(1*time.Second)
+	//os.Exit(1)
+
+}
+
+//Count
+func Count1(ch chan int, i int) {
+	fmt.Println("func:", i)
+	ch <- i	//阻塞操作，被读取之前
+	count1++
+	fmt.Println("Count:", count1, i)
+
+}
---
+++ mysync.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var count int = 0
+
+func main() {
+	lock := &sync.Mutex{}
+	for i := 0; i < 10; i++  {
+		go Count(lock, i)
+	}
+	j := 0
+	for  {
+		lock.Lock()
+		j++
+		fmt.Println("ii=", j, ";count=", count)
+		lock.Unlock()
+		runtime.Gosched() //出让CPU时间片，降低处理优先级
+		if count >= 10{
+			break
+		}
+
+	}
+
+}
+
+func Count(lock *sync.Mutex, i int) {
+	lock.Lock()
+	time.Sleep(1*time.Second)
+	//time.Sleep(1)
+	count++
+	fmt.Println("i=", i, ";count=", count)
+	lock.Unlock()
+}
